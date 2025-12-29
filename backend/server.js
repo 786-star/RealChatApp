3fba06c3d3770af8394588ebd827b7da67ec34ae
Sikandar Ali <sikandrjutt1717@gmail.com>
2025-12-29 11:40:38 +0500

    cors package

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv'
 dotenv.config();
+import cors from 'cors'
 
 const PORT = process.env.PORT
 
 const app = express()
+
+app.use(cors())
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is running at PORT : ${PORT}`)
